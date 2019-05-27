b21f88027f7294051325be1ee04d7ecd41000b1a
rdyashaswi1999 <rdyashaswi1999@gmail.com>
2019-05-27 19:03:08 -0400

    Add project files.

---
+++ lesson3/Controllers/CarsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using lesson3.Models;
+
+namespace lesson3.Controllers
+{
+    public class CarsController : Controller
+    {
+        List<Cars> carList = new List<Cars>()
+        {
+            new Cars {Id = 1, Brand = "Chrysler", Model = "300s", Type = "Luxury Sedan", Year = 2016 },
+            new Cars {Id = 2, Brand = "Hyundai", Model = "Veloster", Type = "Hatchback", Year = 2013 },
+            new Cars {Id = 3, Brand = "Dodge", Model = "Charger", Type = "Sedan", Year = 2017 }
+        };
+    
+    
+
+
+        // GET: Cars
+        public ActionResult Index()
+        {
+            return View(carList);
+        }
+
+
+        // GET: Cars/Details - Single Car
+        public ActionResult Details (int? id)
+        {
+            if (id == null || id > carList.Count)
+            {
+                return Content("Invalid car id");
+
+            }
+            var ind = Convert.ToInt32(id) - 1;
+            var car = carList[ind];
+
+            return View(car);
+
+        }
+
+
+
+    }
+}
---
+++ lesson3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lesson3.MvcApplication" Language="C#" %>
